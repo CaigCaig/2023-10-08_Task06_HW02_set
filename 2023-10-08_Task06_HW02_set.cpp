786099eb78d59698a60e1dae35e02e87f5a8287c
CaigCaig <caig@inbox.ru>
2023-10-08 23:42:47 +0300

    Initial commit

---
+++ 2023-10-08_Task06_HW02_set.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    cout << "[IN]:\n";
+    int n, x;
+    set<int, greater<int>> s;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        s.insert(x);
+    }
+
+    cout << "[OUT]:\n";
+    for (const auto& i : s) cout << i << endl;
+
+    system("pause");
+}
